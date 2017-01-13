10689b44d5595b17cc06bd10c3c5e9a613890388
Marc <mcalafellsmx@gmail.com>
2017-01-13 16:22:05 +0100

    added vue material and navmenu

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,25 @@
 <template>
-  <div>ASDASDASDASDASDASDADASDASDASDASDASDASDASDASD</div>
+  <div class="phone-viewport">
+    <md-toolbar>
+      <md-button class="md-icon-button" @click="toggleLeftSidenav">
+        <md-icon>menu</md-icon>
+      </md-button>
+
+      <h2 class="md-title">Todos</h2>
+    </md-toolbar>
+
+    <md-sidenav class="md-left" ref="leftSidenav" @open="open('Left')" @close="close('Left')">
+      <md-toolbar class="md-large">
+        <div class="md-toolbar-container">
+          <h3 class="md-title">Sidenav content</h3>
+        </div>
+      </md-toolbar>
+
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi cupiditate esse necessitatibus beatae nobis, deserunt ut est fugit, tempora deleniti, eligendi commodi doloribus. Nemo, assumenda possimus, impedit inventore perferendis iusto!</p>
+    </md-sidenav>
+
+  </div>
+
 </template>
 
 <script>
@@ -9,7 +29,25 @@ export default {
   name: 'app',
   components: {
     Hello
+  },
+  methods: {
+    toggleLeftSidenav () {
+      this.$refs.leftSidenav.toggle()
+    },
+    toggleRightSidenav () {
+      this.$refs.rightSidenav.toggle()
+    },
+    closeRightSidenav () {
+      this.$refs.rightSidenav.close()
+    },
+    open (ref) {
+      console.log('Opened: ' + ref)
+    },
+    close (ref) {
+      console.log('Closed: ' + ref)
+    }
   }
+
 }
 </script>
 
