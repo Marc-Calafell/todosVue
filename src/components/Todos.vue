1425a60eeee32a1b19d79d05514538cb8f418340
Marc <mcalafellsmx@gmail.com>
2017-01-18 19:49:27 +0100

    Me cago en la merda esta del format del codi

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -2,13 +2,13 @@
     <div>
         <div v-show="!authorized">
             <md-button class="md-raised md-primary" @click="connect">Connect</md-button>
+
             <div>
-                <ul>
-                    <li v-for="(todo, index) in todos">
-                        {{ todo.name }}
-                    </li>
-                </ul>
+                <ul><li v-for="(todo, index) in todos">{{ todo.name }}</li></ul>
+
             </div>
+
+
         </div>
     </div>
 </template>
@@ -18,51 +18,65 @@
   }
 </style>
 <script>
-var STOARGE_KEY =todosvue_token
-  export default{
-    data () {
-      return {
-        todos: [],
-        authorized: false
-      }
+var STORAGE_KEY = 'todosvue_token'
+var AUTH_CLIENT_ID = 1
+var AUTH_REDIRECT_API = 'http://oauthclient.dev:8081/implicit'
+
+export default{
+  data () {
+    return {
+      todos: [],
+      authorized: false
+    }
+  },
+  created () {
+    console.log(this.fetchToken())
+    if (this.fetchToken()) {
+      this.authorized = true
+    } else {
+      this.authorized = false
+    }
+  },
+  methods: {
+    fetchData: function () {
+      return this.fetchPage(1)
     },
-    created () {
-      console.log(this.fetchToken())
-      if (this.fetchToken()) {
-        this.authorized = true
-      } else {
-        this.authorized = false
-      }
+
+    fetchPage: function (page) {
+      this.$http.get('http://todos.dev:8000/api/v1/task?page' + page).then((response) => {
+        console.log(response.data)
+        this.todos = response.data.data
+      }, (response) => {
+        console.log('Error')
+      })
     },
-    methods: {
-      fetchData: function () {
-        return this.fetchPage(1)
-      },
-      fetchPage: function (page) {
-        this.$http.get('http://todos.dev:8000/api/v1/task?page' + page).then((response) => {
-          console.log(response.data)
-          this.todos = response.data.data
-        }, (response) => {
-          console.log('Error')
-        })
-      },
-      connect: function () {
-        console.log('Connect here');
-        'client_id'=> '3',
-        'redirect_uri' => 'http://oauthclient.dev:8081/implicit',
-        'response_type' => 'token', //implicit
-        'scope' => '',
-        var query = 'client_id=' + AUTH_CLIENT_ID + '&redirect_uri=' + window.location + '&response_type=token&scope='
-         console.log(query)
-        window.location.replace('http://todos.dev:8080/oauth/authorize?' + query)
-      },
-      fetchToken: function () {
-        return window.localStorage.getItem(window.STORAGE_KEY)
-      },
-      save: function (toKEN) {
-        window.localStorage.setItem(window.STORAGE_KEY, this.token)
+    connect: function () {
+       // console.log('Connect here')
+      query = {
+        client_id: AUTH_CLIENT_ID,
+        redirect_uri: AUTH_REDIRECT_API,
+        response_type: 'token', // implicit
+        scope: ''
       }
+
+      var query = 'client_id=' + AUTH_CLIENT_ID + '&redirect_uri=' + window.location + '&response_type=token&scope='
+      console.log(query)
+      window.location.replace('http://todos.dev:8080/oauth/authorize?' + query)
+    },
+
+    fetchToken: function () {
+      return window.localStorage.getItem(window.STORAGE_KEY)
+    },
+
+    save: function (token) {
+      window.localStorage.setItem(window.STORAGE_KEY, this.token)
+    },
+
+    logout: function () {
+      window.localStorage.removeItem(STORAGE_KEY)
+      this.authorized(false)
     }
   }
+}
 
 </script>
